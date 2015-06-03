ca864a63cfae1391a06c68e9d898188a5d48476f
marcojetson <marcojetson@gmail.com>
2015-06-03 20:28:11 +0200

    Initial Commit

---
+++ Messenger/AppDelegate.m
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.m
+//  Messenger
+//
+//  Created by Marco on 6/3/15.
+//  Copyright (c) 2015 marcojetson. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@interface AppDelegate ()
+
+@property (weak) IBOutlet NSWindow *window;
+@end
+
+@implementation AppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
+    // Insert code here to initialize your application
+}
+
+- (void)applicationWillTerminate:(NSNotification *)aNotification {
+    // Insert code here to tear down your application
+}
+
+@end
---
+++ Messenger/main.m
@@ -0,0 +1,13 @@
+//
+//  main.m
+//  Messenger
+//
+//  Created by Marco on 6/3/15.
+//  Copyright (c) 2015 marcojetson. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    return NSApplicationMain(argc, argv);
+}
